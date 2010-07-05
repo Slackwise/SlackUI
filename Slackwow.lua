a9b75df8a4cf2f0a2fb9d00f29d09d631c227be3
Slackwise <slackwise@gmail.com>
2010-07-05 11:56:49 -0500

    Added proper global constants.

--- Slackwow.lua
+++ Slackwow.lua
@@ -17,8 +17,8 @@ end
 
 --Sell gray items. Adapted from tekJunkSeller.
 function event:MERCHANT_SHOW()
-	for bag=0,4 do
-		for slot=0,GetContainerNumSlots(bag) do
+	for bag = 0, NUM_BAG_SLOTS do
+		for slot = 0, GetContainerNumSlots(bag) do
 			local link = GetContainerItemLink(bag, slot)
 			if link and select(3, GetItemInfo(link)) == 0 then
 				ShowMerchantSellCursor(1)

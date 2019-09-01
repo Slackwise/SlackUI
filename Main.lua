bcac05e38a1cb2aa573f4310f943888b21ef312b
Adam Flanczewski <sl@ckwi.se>
2019-09-01 10:07:24 -0500

    Fixed gray item auto-selling for Classic and newer.

--- Main.lua
+++ Main.lua
@@ -28,7 +28,6 @@ function addon:MERCHANT_SHOW()
 		for slot = 0, GetContainerNumSlots(bag) do
 			local link = GetContainerItemLink(bag, slot)
 			if link and select(3, GetItemInfo(link)) == 0 then
-				ShowMerchantSellCursor(1)
 				UseContainerItem(bag, slot)
 			end
 		end

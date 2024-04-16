35e028f05847eae21b73c9dd8c42012175c2f1da
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2024-04-16 11:25:28 -0500

    Added StaticData.lua

---
+++ StaticData.lua
@@ -0,0 +1,10 @@
+setfenv(1, _G.SlackUI)
+
+HEALING_POTION_MAX_HEALING = {
+-- ITEM_ID = MAX_HEALING
+  [207023] = 310592, -- Dreamwalker's Healing Potion (Quality 3)
+  [207022] = 266709, -- Dreamwalker's Healing Potion (Quality 2)
+  [207021] = 228992, -- Dreamwalker's Healing Potion (Quality 1)
+}
+
+MANA_POTION_MAX_HEALING = {}

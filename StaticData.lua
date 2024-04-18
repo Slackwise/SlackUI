a0bce9f978b584b89fb276ec855866578238dd78
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2024-04-18 08:20:33 -0500

    StaticData: Mapped DRAGONFLIGHT and CLASSIC healing potions

--- StaticData.lua
+++ StaticData.lua
@@ -2,9 +2,19 @@ setfenv(1, _G.SlackUI)
 
 HEALING_POTION_MAX_HEALING = {
 -- ITEM_ID = MAX_HEALING
-  [207023] = 310592, -- Dreamwalker's Healing Potion (Quality 3)
-  [207022] = 266709, -- Dreamwalker's Healing Potion (Quality 2)
-  [207021] = 228992, -- Dreamwalker's Healing Potion (Quality 1)
+
+  -- Dragonflight Potions:
+  [207023]  = 310592, -- Dreamwalker's Healing Potion (Quality 3)
+  [207022]  = 266709, -- Dreamwalker's Healing Potion (Quality 2)
+  [207021]  = 228992, -- Dreamwalker's Healing Potion (Quality 1)
+
+  -- Classic Potions:
+  [13446]   = 1750,   -- Major Healing Potion
+  [3928]    = 900,    -- Superior Healing Potion
+  [1710]    = 585,    -- Greater Healing Potion
+  [929]     = 360,    -- Healing Potion
+  [858]     = 180,    -- Lesser Healing Potion
+  [118]     = 90,     -- Minor Healing Potion
 }
 
 MANA_POTION_MAX_HEALING = {}

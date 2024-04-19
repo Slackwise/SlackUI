b9a9e06fef53f419fc9480ab7db101f6c466627a
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2024-04-19 00:38:39 -0500

    Corrected `groupBy` to operate on array.

--- Main.lua
+++ Main.lua
@@ -238,14 +238,14 @@ function findItemsByRegex(regex)
   return found
 end
 
---- Invert a table by grouping the keys by the values.
+--- Group array items into a table by the output of a function.
 ---@param Array - The array to group.
----@param Function(key, value) - Takes in a `key` and `value`, and returns new `key, value` which will be grouped by `key`.
+---@param Function(object) - Takes in an object, and returns new `key, value` which will be grouped by `key`.
 ---@return Array - The grouped array.
 function groupBy(array, groupingFunction)
   local grouped = {}
-  for key, value in ipairs(array) do
-    local groupingKey, newValue = groupingFunction(key, value)
+  for i, value in ipairs(array) do
+    local groupingKey, newValue = groupingFunction(value)
     if grouped[value] then
       table.insert(grouped[groupingKey], newValue)
     else

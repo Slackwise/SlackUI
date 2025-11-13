3e623ac570906f292e684e01610dac86cc80ff47
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2025-11-13 13:43:36 -0600

    Updated powershell script to junction beta directory too

--- Add-AddonFolderLinks.ps1
+++ Add-AddonFolderLinks.ps1
@@ -1,32 +1,33 @@
 $sourceDir    = "$PSScriptRoot"
-$retailDir    = "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\SlackwiseTweaks"
-$classicDir   = "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\SlackwiseTweaks"
+$dirs = @{
+    retail = "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\SlackwiseTweaks"
+    classic = "C:\Program Files (x86)\World of Warcraft\_classic_era_\Interface\AddOns\SlackwiseTweaks"
+    beta = "C:\Program Files (x86)\World of Warcraft\_beta_\Interface\AddOns\SlackwiseTweaks"
+}
 
 # Check if script is running as administrator, if not, relaunch as administrator
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Host "Script is not running as administrator. Relaunching with administrator privileges..."
+    Write-Error "Script is not running as administrator. Relaunching with administrator privileges..."
     Start-Process powershell.exe -Verb RunAs -ArgumentList ("-File", $MyInvocation.MyCommand.Path)
     exit
 }
 
-try {
-    New-Item -ItemType Junction -Path $retailDir -Target $sourceDir -ErrorAction Stop
-    Write-Host "Junctioned retail dir."
-} catch [System.IO.IOException] {
-    Write-Host "Retail dir already exists!"
-} catch {
-    Write-Host "Error while trying to junction retail dir:"
-    Write-Host $_
-}
-
-try {
-    New-Item -ItemType Junction -Path $classicDir -Target $sourceDir -ErrorAction Stop
-    Write-Host "Junctioned classic dir."
-} catch [System.IO.IOException] {
-    Write-Host "classic dir already exists!"
-} catch {
-    Write-Host "Error while trying to junction classic dir:"
-    Write-Host $_
+foreach ($dirName in $dirs.Keys) {
+    $parentDir = Split-Path -Parent $dirs[$dir]
+    if (-not (Test-Path -Path $parentDir)) {
+        Write-Host "Skipping $dirName: directory does not exist"
+        continue
+    }
+    
+    try {
+        New-Item -ItemType Junction -Path $dirs[$dirName] -Target $sourceDir -ErrorAction Stop
+        Write-Host "Junctioned $dirName directory."
+    } catch [System.IO.IOException] {
+        Write-Error "$dirName directory already exists!"
+    } catch {
+        Write-Error "Error while trying to junction $dirName directory:"
+        Write-Error $_
+    }
 }
 
 pause
